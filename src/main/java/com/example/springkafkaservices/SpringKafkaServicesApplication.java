4e5de28fdf5e8c626b3141d27d161d41aaddf6d9
apicik <apicik@ya.ru>
2021-09-18 15:52:09 +0400

    f

---
+++ src/main/java/com/example/springkafkaservices/SpringKafkaServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkaservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springkafkaservices/SpringKafkaServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkafkaservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
